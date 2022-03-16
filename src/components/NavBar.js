cc8fa2dfeac20618ec22fb930c49c33b2e74b978
mkarraz <matias.karraz@gmail.com>
2022-03-16 00:07:58 -0300

    Desafio_Clase_2

---
+++ src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Nav, Navbar, NavDropdown, Container, NavLink } from 'react-bootstrap'
+import { IoCartOutline } from "react-icons/io5";
+import logo from '../img/logo.png'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <div>
+                    <Navbar.Brand href="#home">
+                        <img
+                        src={logo}
+                        width="170"
+                        height="80"
+                        className="d-inline-block align-top"
+                        alt="RacketStore logo"
+                        />
+                    </Navbar.Brand>
+                </div>
+                <div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#store">Store</Nav.Link>
+                            <NavDropdown title="Brands" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Babolat</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Head</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Wilson</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Accessories</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown.Divider/>
+                            <Nav.Link href="#store"><IoCartOutline/> Shopping Cart</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
